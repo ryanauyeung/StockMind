76c8d006dd3db9b1db48cadf560014d35ec49993
ryanauyeung <auwang@gmail.com>
2026-09-20 12:42:57 +0800

    chore: remove old trendline models/__init__.py

--- src/trendline/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from trendline.models.baseline import BaselineModel
-from trendline.models.families import SectorBundle, SharedBundle, StockBundle
-from trendline.models.lightgbm_quantile import QuantileLGBM
-
-__all__ = ["BaselineModel", "QuantileLGBM", "SharedBundle", "SectorBundle", "StockBundle"]
